18cd904a74c996e772c7744e327e16466ba74bb7
MrNickell <songtianyou07@gmail.com>
2021-11-22 20:12:15 -0500

    Add admin pages

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,17 @@
 import VueRouter from 'vue-router'
 import MainLayout from '~/layouts/MainLayout'
+import AdminLayout from '~/layouts/AdminLayout'
 import User from '~/components/User'
 import Article from '~/components/article/Article'
 import ArticleDetail from '~/components/article/ArticleDetail'
 import ArticleEdit from '~/components/article/ArticleEdit'
+
+import AdminLogin from '~/components/Admin/AdminLogin'
+import AdminArticles from '~/components/Admin/AdminArticles'
+import AdminComments from '~/components/Admin/AdminComments'
+import AdminSections from '~/components/Admin/AdminSections'
+import AdminUsers from '~/components/Admin/AdminUsers'
+
 import Address from '~/components/Address'
 import Profile from '~/components/Profile'
 import GoogleLoginCallback from '~/components/GoogleLoginCallback'
@@ -34,6 +42,36 @@ const routes = [
         },{
             path: '/profile',
             component: Profile
+        }]
+    },
+    {
+        path: '/admin/',
+        redirect: '/admin/login',
+        component: AdminLayout,
+        children: [{
+            path: 'login',
+            name: 'AdminLogin',
+            component: AdminLogin
+        },
+        {
+            path: 'articles',
+            name: 'ArticleList',
+            component: AdminArticles
+        },
+        {
+            path: 'users',
+            name: 'UserList',
+            component: AdminUsers
+        },
+        {
+            path: 'comments',
+            name: 'CommentList',
+            component: AdminComments
+        },
+        {
+            path: 'sections',
+            name: 'SectionList',
+            component: AdminSections
         }]
     }
 ]
